95373f85beec8a7d2abe6e9f7e3f7127958b927b
Damjan83 <damjanpokrajac83@gmail.com>
2021-12-05 13:46:35 +0100

    my changes

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const thumbActive = document.querySelectorAll('.thumb-img');
+
 for(let i = 0; i < thumbActive.length; i++){
-    thumbActive[i].addEventListener('click' , () => { 
+    thumbActive[i].addEventListener('click' , (e) => { 
         const thumb = 'container__thumbnails-img-' + [i+1];
         const thumbImg = thumb.slice(-1);
+    
         document.querySelector('.product').src = './images/image-product-' + thumbImg + '.jpg';
-        thumbActive[i].classList.add('is-active');  
+
+        thumbActive.forEach((i) => {
+            i.classList.remove('is-active');
+        });
+
+        thumbActive[i].classList.add('is-active');
     });
-   
-  
 };
 
 
